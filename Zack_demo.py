389d7abc4285ed280e8c12d07fae53b6c5c11084
Zackary Savoie <19zs51@queensu.ca>
2023-03-18 00:11:42 -0400

    REALLY cool demo in zack_demo.py

--- Zack_demo.py
+++ Zack_demo.py
@@ -1,13 +1,21 @@
 import h5py as h5
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.widgets import Slider, Button
 import math
 
-def y(i):
-    return dataset1[int(i)*wS[1]:(int(i)+1)*wS[1],4]
+def y0(i):
+    return dataset0[int(i*w):int((i+1)*w),4]
+
+def y1(i):
+    return dataset1[int(i*w):int((i+1)*w),4]
 
 def update(val):
-    line.set_ydata(y(freq_slider.val))
+    line.set_ydata(y0(slider.val))
+    line2.set_ydata(y1(slider.val))
+    marker.set_xdata(30 + (val-5.5)*(30/5.5))
+    marker2.set_xdata(30 + (val - 5.5) * (30 / 5.5))
+    print(str((val*w)*s) + "s to " + str(((1+val)*w)*s) + "s")
     fig.canvas.draw_idle()
 
 # Open HDF5
@@ -15,32 +23,42 @@ HDF5File = h5.File("data.hdf5", 'r')
 
 keyList = list([key for key in HDF5File["dataset/Train"].keys()])
 dataset0 = HDF5File["Jillian"]["0-Jillian"]  # Walking
-dataset1 = HDF5File["Jillian"]["6-Jillian"]  # Jumping
+dataset1 = HDF5File["Jillian"]["8-Jillian"]  # jumping
 
-wS = [int(math.floor(dataset0.shape[0]/12)), int(math.floor(dataset1.shape[0]/12))]  # Window size (~5s)
-i = 4
+w = int(dataset0.shape[0]/12)
+s = 60/dataset0.shape[0]
 
 fig, ax = plt.subplots(2,2)
-line, = ax[0,0].plot(dataset0[:,0], dataset0[:,4])
-line, = ax[0,1].plot(dataset1[:,0], dataset1[:,4])
 
-line, = ax[1,0].plot(dataset0[2*wS[0]:(2+1)*wS[0],0], dataset0[2*wS[0]:(2+1)*wS[0],4])
-line, = ax[1,1].plot(dataset1[4*wS[1]:(4+1)*wS[1],0], dataset1[2*wS[1]:(2+1)*wS[1],4])
-fig.suptitle('Plot of ' + str(keyList[6]), fontsize=16)
+for j in range(0,2):
+    ax[0,j].set_ylim(-5, 70)
+    ax[0,j].set_xlim(0, 60)
+
+for j in range(0,2):
+    ax[1,j].set_ylim(-5, 70)
+
+ax[0,0].plot(dataset0[:,0], dataset0[:,4])
+ax[0,1].plot(dataset1[:,0], dataset1[:,4])
+
+line, = ax[1,0].plot(dataset0[5*w:6*w,0], dataset0[5*w:6*w,4])
+line2, = ax[1,1].plot(dataset1[5*w:6*w,0], dataset1[5*w:6*w,4])
+fig.suptitle('Comparaison of walking and jumping, sliding window' + str(keyList[6]), fontsize=16)
+
+[marker] = ax[0,0].plot((30,30), (-5,70), scaley = False)
+[marker2] = ax[0,1].plot((30,30), (-5,70), scaley = False)
 
 
 
-
-axfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-freq_slider = Slider(
-    ax=axfreq,
+axslider = fig.add_axes([0.175, 0.1, 0.65, 0.02])
+slider = Slider(
+    ax=axslider,
     label='i',
     valmin=0,
     valmax=11,
-    valinit=5,
+    valinit=5.5,
 )
 
-freq_slider.on_changed(update)
-
+slider.on_changed(update)
+fig.subplots_adjust(bottom=0.25)
 plt.show()
 
